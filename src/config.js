15650adbf40905e363894625e864e9695e4ea380
John Jiang <john.jiang@checkit.net>
2020-05-10 15:30:37 +0100

    Add heroku deployment

--- src/config.js
+++ src/config.js
@@ -3,7 +3,10 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'dev';
 const configs = {
 	dev: {
 		port: 3009,
-	},
+  },
+  production: {
+    port: 3000
+  }
 };
 
 module.exports = configs[env];
